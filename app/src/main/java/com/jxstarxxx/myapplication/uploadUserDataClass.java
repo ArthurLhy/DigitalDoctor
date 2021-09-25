cd4eef9a291b9eae569e5318df6fb25da8095a5d
Shiyi Xu <xus3@student.unimelb.edu.au>
2021-09-25 17:13:29 +1000

    register and login in function

---
+++ app/src/main/java/com/jxstarxxx/myapplication/uploadUserDataClass.java
@@ -0,0 +1,17 @@
+package com.jxstarxxx.myapplication;
+
+public class uploadUserDataClass {
+    public String profilePicUrl;
+
+    public uploadUserDataClass(String profilePicUrl) {
+        this.profilePicUrl = profilePicUrl;
+    }
+
+    public String getProfilePicUrl() {
+        return profilePicUrl;
+    }
+
+    public void setProfilePicUrl(String profilePicUrl) {
+        this.profilePicUrl = profilePicUrl;
+    }
+}
---
+++ app/src/main/java/com/jxstarxxx/myapplication/uploadUserDataJavaClass.java
@@ -0,0 +1,20 @@
+package com.jxstarxxx.myapplication;
+
+public class uploadUserDataJavaClass {
+    public String userProfilePicUrl;
+
+    public uploadUserDataJavaClass() {
+    }
+
+    public uploadUserDataJavaClass(String userProfilePicUrl) {
+        this.userProfilePicUrl = userProfilePicUrl;
+    }
+
+    public String getUserProfilePicUrl() {
+        return userProfilePicUrl;
+    }
+
+    public void setUserProfilePicUrl(String userProfilePicUrl) {
+        this.userProfilePicUrl = userProfilePicUrl;
+    }
+}
