714559a0c5e2ebe1a8034e22c96c7d18a58bd1cc
ArthurLhy <Arthurliuhy@gmail.com>
2021-09-27 19:44:29 +0800

    dev

--- app/src/main/java/com/example/medicalchat/MainActivity.java
+++ app/src/main/java/com/example/medicalchat/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.medicalchat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText vPhoneNumber, vCode;
+    private Button vSendCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPhoneNumber = findViewById(R.id.phoneNumber);
+        vCode = findViewById(R.id.verificationCode);
+        vSendCode = findViewById(R.id.sendCode);
+        
     }
 }
