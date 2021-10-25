9514826d1c21e0f3a374ecccd430eb4be9f4a2c0
ruichenaero <chenruiaero@outlook.com>
2021-10-25 13:40:34 +0800

    case tracker

---
+++ app/src/main/java/com/jxstarxxx/myapplication/CovidCasesTeacker.java
@@ -0,0 +1,14 @@
+package com.jxstarxxx.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CovidCasesTeacker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_cases_teacker);
+    }
+}
